2f4a35da1041911739f993a6c09f348f943a1caa
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-13 21:31:29 -0800

    function runs

---
+++ app.js
@@ -0,0 +1,18 @@
+import isYes from '/isYes.js';
+const quizbutton = document.getElementById('quiz');
+const total = document.getElementById('total');
+
+quizbutton.addEventListener('click', () =>{
+
+    const Name = prompt('What is your name?');
+    const confirmation = confirm(`{$Name},Are you sure you are prepared to take the quiz?`);
+    if (confirmation === false) return;
+    const answer1 = prompt('Do you like zebra?');
+    const answer2 = prompt('Zebra don\'t drink water, Right?');
+    const answer3 = prompt('Are Zebra cute?');
+    let count = 0;
+    if (isYes(answer1)) count += 1;
+    if (!isYes(answer2)) count += 1;
+    if (isYes(answer3)) count += 1;
+    total.textContent = `${Name}, you got ${count} correct`;
+});
