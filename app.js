81cdc90ec7bf3d32c3fd7f2181c013d205d0b786
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-15 10:12:56 -0800

    add compareN

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import compareNum from '/compareNum.js';
 const GuessButton = document.getElementById('guessbutton');
 const inputElement = document.getElementById('yournum');
-const correctNumber = Math.floor(Math.random() * 10) ;
-const guess = inputElement.value;
+const correctNumber = Math.floor(Math.random() * 20) ;
 const chance_remain = document.getElementById('remaining');
 //add if function if input>computerNumner{tooHightooLow.textContent ='The number is too high'}
 const tooHightooLow = document.getElementById('hint');
@@ -13,13 +12,16 @@ let chances = 5;
 
 
 GuessButton.addEventListener('click', () =>{
+    const guess = Number(inputElement.value);
     chances--;
     chance_remain.textContent = chances;
-    //console.log(computerNumber);
-    if (compareNum === '1') {
+    //console.log(correctNumber);
+    //console.log(guess);
+    //console.log(compareNum(guess, correctNumber));
+    if (compareNum(guess, correctNumber) === 1) {
         //console.log('angela');
         tooHightooLow.textContent = `Your number is too big`;
-    } else if (compareNum === '-1') {
+    } else if (compareNum(guess, correctNumber) === -1) {
         tooHightooLow.textContent = `Your number is too small`;
     } else {
         tooHightooLow.textContent = `You are right!`;
